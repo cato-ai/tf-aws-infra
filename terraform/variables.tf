295f6cafd30696d973b0aac626ae2761eafd368f
Sampurna <sampurng@gmail.com>
2024-10-10 04:48:15 -0400

    Files added with correct formatting

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
-variable "subnet_0_az"{
-    type = string
-    default = "us-east-1a"
-    description = "Subnet Availability Zone - 0"
+variable "subnet_0_az" {
+  type        = string
+  default     = "us-east-1a"
+  description = "Subnet Availability Zone - 0"
 }
 
-variable "subnet_1_az"{
-    type = string
-    default = "us-east-1b"
-    description = "Subnet Availability Zone - 1"
+variable "subnet_1_az" {
+  type        = string
+  default     = "us-east-1b"
+  description = "Subnet Availability Zone - 1"
 }
 
-variable "subnet_2_az"{
-    type = string
-    default = "us-east-1c"
-    description = "Subnet Availability Zone - 2"
+variable "subnet_2_az" {
+  type        = string
+  default     = "us-east-1c"
+  description = "Subnet Availability Zone - 2"
 }
