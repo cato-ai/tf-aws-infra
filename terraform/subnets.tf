6abe07f44472f14941fbbfd0ff3007117c79c10c
Sampurna <sampurng@gmail.com>
2024-10-10 04:19:06 -0400

    Added files for tf

---
+++ terraform/subnets.tf
@@ -0,0 +1,68 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "csye6225_subnet_0_public" {
+  cidr_block              = cidrsubnet("10.10.0.0/16", 8, 0)
+  map_public_ip_on_launch = "true"
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  tags = {
+    Name = "csye6225_subnet_0_public"
+  }
+  availability_zone = var.subnet_0_az
+}
+
+resource "aws_subnet" "csye6225_subnet_0_private" {
+  cidr_block              = cidrsubnet("10.10.0.0/16", 8, 1)
+  map_public_ip_on_launch = "false"
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  tags = {
+    Name = "csye6225_subnet_0_private"
+  }
+  availability_zone = var.subnet_0_az
+}
+
+resource "aws_subnet" "csye6225_subnet_1_public" {
+  cidr_block              = cidrsubnet("10.10.0.0/16", 8, 2)
+  map_public_ip_on_launch = "true"
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  tags = {
+    Name = "csye6225_subnet_1_public"
+  }
+  availability_zone = var.subnet_1_az
+
+}
+
+resource "aws_subnet" "csye6225_subnet_1_private" {
+  cidr_block              = cidrsubnet("10.10.0.0/16", 8, 3)
+  map_public_ip_on_launch = "false"
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  tags = {
+    Name = "csye6225_subnet_1_private"
+  }
+  availability_zone = var.subnet_1_az
+}
+
+resource "aws_subnet" "csye6225_subnet_2_public" {
+  cidr_block              = cidrsubnet("10.10.0.0/16", 8, 4)
+  map_public_ip_on_launch = "true"
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  tags = {
+    Name = "csye6225_subnet_2_public"
+  }
+  availability_zone = var.subnet_2_az
+}
+
+resource "aws_subnet" "csye6225_subnet_2_private" {
+  cidr_block              = cidrsubnet("10.10.0.0/16", 8, 5)
+  map_public_ip_on_launch = "false"
+  vpc_id                  = aws_vpc.csye6225_vpc.id
+  tags = {
+    Name = "csye6225_subnet_2_private"
+  }
+  availability_zone = var.subnet_2_az
+}
+
+
+
+
---
+++ terraform/vpc.tf
@@ -0,0 +1,64 @@
+resource "aws_vpc" "csye6225_vpc" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "csye6225_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "csye6225_igw" {
+  vpc_id = aws_vpc.csye6225_vpc.id
+}
+
+resource "aws_route_table" "csye6225_public_rt" {
+  vpc_id = aws_vpc.csye6225_vpc.id
+}
+resource "aws_route_table" "csye6225_private_rt" {
+  vpc_id = aws_vpc.csye6225_vpc.id
+}
+
+
+resource "aws_route_table_association" "associate_public_0" {
+  subnet_id      = aws_subnet.csye6225_subnet_0_public.id
+  route_table_id = aws_route_table.csye6225_public_rt.id
+}
+
+resource "aws_route_table_association" "associate_public_1" {
+  subnet_id      = aws_subnet.csye6225_subnet_1_public.id
+  route_table_id = aws_route_table.csye6225_public_rt.id
+}
+
+resource "aws_route_table_association" "associate_public_2" {
+  subnet_id      = aws_subnet.csye6225_subnet_2_public.id
+  route_table_id = aws_route_table.csye6225_public_rt.id
+}
+
+resource "aws_route" "public_route_table_igw_associtaion" {
+  route_table_id         = aws_route_table.csye6225_public_rt.id  
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.csye6225_igw.id  
+}
+
+resource "aws_route_table_association" "associate_igw" {
+  gateway_id      = aws_internet_gateway.csye6225_igw.id
+  route_table_id = aws_route_table.csye6225_public_rt.id
+}
+
+resource "aws_route_table_association" "associate_private_0" {
+  subnet_id      = aws_subnet.csye6225_subnet_0_private.id
+  route_table_id = aws_route_table.csye6225_private_rt.id
+}
+
+resource "aws_route_table_association" "associate_private_1" {
+  subnet_id      = aws_subnet.csye6225_subnet_1_private.id
+  route_table_id = aws_route_table.csye6225_private_rt.id
+}
+
+resource "aws_route_table_association" "associate_private_2" {
+  subnet_id      = aws_subnet.csye6225_subnet_2_private.id
+  route_table_id = aws_route_table.csye6225_private_rt.id
+}
+
+
+
+
+
